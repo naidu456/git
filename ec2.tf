2fe510d841579a770c6aa69a86ea289a47b5a470
naidu456 <118648202+naidu456@users.noreply.github.com>
2023-02-07 14:34:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "demoinstance" {
 ami                         = "ami-0cca134ec43cf708f"
 instance_type               = "t2.micro"
 #count                       = 1
-key_name                    = "sankranthi"
+key_name                    = "two"
 vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
 subnet_id                   = aws_subnet.public-subnet-1.id
 associate_public_ip_address = true
@@ -17,7 +17,7 @@ resource "aws_instance" "demoinstance1" {
 ami                         = "ami-0cca134ec43cf708f"
 instance_type               = "t2.micro"
 #count                       = 1
-key_name                    = "sankranthi"
+key_name                    = "two"
 vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
 subnet_id                   = aws_subnet.public-subnet-2.id
 associate_public_ip_address = true
